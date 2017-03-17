e6fee15fdd5367eb12f33bebf955c74ae71530f7
Jarven <769558579@qq.com>
2017-03-17 14:38:34 +0800

    RESTful  统一封装类DataRet

--- src/main/java/com/kunlun/common/DataRet.java
+++ src/main/java/com/kunlun/common/DataRet.java
@@ -1,8 +1,55 @@
 package com.kunlun.common;
 
+import java.io.Serializable;
+
 /**
+ *
+ *
  * Created by kunlun on 2017/3/17.
  */
-public class DataRet {
+public class DataRet<T> implements Serializable {
 
+    private static final long serialVersionUID = 7913676418715973680L;
+    //错误编码
+    private String errorCode;
+
+    //消息
+    private String message;
+
+    //返回信息体
+    private T body;
+
+    public DataRet() {
+
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getBody() {
+        return body;
+    }
+
+    public void setBody(T body) {
+        this.body = body;
+    }
+
+    public DataRet(String errorCode, String message, T body) {
+        this.errorCode = errorCode;
+        this.message = message;
+        this.body = body;
+    }
 }
--- src/main/java/com/kunlun/web/IndexController.java
+++ src/main/java/com/kunlun/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kunlun.web;
 
+import com.kunlun.common.DataRet;
 import com.kunlun.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +14,22 @@ public class IndexController {
 
     @Autowired
     private UserService userService;
+
     @RequestMapping("/index")
-    public String index(){
-        userService.create("1","张三");
-        userService.create("2","李四");
-        userService.create("3","王五");
+    public String index() {
+        userService.create("1", "张三");
+        userService.create("2", "李四");
+        userService.create("3", "王五");
         return "hello world!";
     }
+
     @RequestMapping("/json")
-    public String json(){
-        return null;
+    public DataRet<String> json() {
+        DataRet<String> ret = new DataRet<>();
+        ret.setBody("Null");
+        ret.setErrorCode("400");
+        ret.setMessage("Nothing...");
+        return ret;
     }
 
 
