6920ef6dd7e26f5fa2311faaa6103dfcc112e734
Jarven <769558579@qq.com>
2017-03-17 13:43:34 +0800

    注册拦截器

--- src/main/java/com/kunlun/config/WebMvcConfig.java
+++ src/main/java/com/kunlun/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.kunlun.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,12 +14,27 @@ import java.util.List;
 /**
  * Created by kunlun on 2017/3/17.
  */
+//配置声明
 @Configuration
+//开启MVC模式
 @EnableWebMvc
+//包扫描
+@ComponentScan()
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     /**
+     * 登录拦截器
+     *
+     * @return
+     */
+    @Bean
+    private LoginIntercepter loginIntercepter() {
+        return new LoginIntercepter();
+    }
+
+    /**
      * JSON转换
+     *
      * @param converters
      */
     @Override
@@ -30,6 +47,9 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         super.addViewControllers(registry);
     }
 
-
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.loginIntercepter());
+        super.addInterceptors(registry);
+    }
 }
