ce20c24a50be93aad56d32b204ee9a03b39df3ca
Jarven <769558579@qq.com>
2017-03-17 10:12:29 +0800

    配置模板引擎  thymeleaf 配置静态资源图片访问

---
+++ src/main/java/com/kunlun/web/ThymeleafController.java
@@ -0,0 +1,18 @@
+package com.kunlun.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kunlun on 2017/3/17.
+ */
+@Controller
+public class ThymeleafController {
+
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(ModelMap map) {
+        map.addAttribute("host", "localhost:8080");
+        return "index";
+    }
+}
