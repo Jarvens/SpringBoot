df224aa4a4749292941c57a4781632431651d8b9
Jarven <769558579@qq.com>
2017-03-17 09:59:22 +0800

    helloworld 启动

---
+++ src/main/java/com/kunlun/Application.java
@@ -0,0 +1,14 @@
+package com.kunlun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kunlun on 2017/3/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/kunlun/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.kunlun.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by kunlun on 2017/3/17.
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "hello world!";
+    }
+}
